3ed0033aa8e93f16fefc230c746cad075d0b84d5
Jesus S Junior <jesus.sjunior@totvs.com.br>
2025-11-17 12:10:40 -0300

    criação do componente  tree

---
+++ src/app/category-tree/category-tree.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-tree',
+  imports: [],
+  templateUrl: './category-tree.component.html',
+  styleUrl: './category-tree.component.css'
+})
+export class CategoryTreeComponent {
+
+}
